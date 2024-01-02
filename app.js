8270bb5c1da7ca17d387e77938736f23fd0b845b
Alex <aj.perez@live.com>
2024-01-01 19:28:47 -0500

    Structured JS into categories, set state and constants

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // constants
+const total_cards =16
+const card_images = [ ]
 
 // variables (state)
+let flippedCards = []
+let matchedCards = []
+let timer =0
+let time = 0
+let isFlipped = false
 
 // Cached element references
 
