5ebd36d5cc247377b8cb6129a93db7d9391f1a1c
Alex <aj.perez@live.com>
2024-01-02 00:07:53 -0500

    linked array to match images, fisher-yates function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // constants
-const total_cards =16
-const card_images = [ ]
+const total cards =16
+const card images = [ ]
 
 // variables (state)
 let flippedCards = []
@@ -22,7 +22,26 @@ restartButton.addEventListener(`click`, restartGame)
 
 // Functions
 function intializeGame(){
-  
+  const shuffledCards = shuffleArray(shuffledImages))
+}
+shuffledImages.push(cardImages)
+shuffledImages.push(cardImages)
+shuffleArray(shuffledImages)
+// linking card images with itself will create pairs of images for the game.
+for (let i = 0; i < shuffledImages.length; i++) {
+  const card = document.createElement('div')
+  card.classList.add('card')
+  card.setAttribute('data-index', i)
+  card.style.backgroundImage = `url(${shuffledImages[i]})`
+  gameBoard.appendChild(card);
+}
+// Function for shufflings (Fisher-Yates shuffle algorithm)
+function shuffleArray(array) {
+for (let i = array.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1))
+  [array[i], array[j]] = [array[j], array[i]]
+}
+return array;
 }
 function checkForMatch(){
 
