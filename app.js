15c06425da31c37490d7941d23614077e8fd13ea
Alex <aj.perez@live.com>
2024-01-01 20:37:50 -0500

    listed functions, event listeners,cached elements

--- app.js
+++ app.js
@@ -10,7 +10,35 @@ let time = 0
 let isFlipped = false
 
 // Cached element references
+const Game = document.querySelector(`Game`)
+const timerDisplay = document.getElementById(`time`)
+const winMessage = document.getElementById (`Win`)
+const lossMessage = document.getElementById (`Lose`)
+const restartButton = document.getElementById (`restart button`)
 
 // event listeners 
+Game.addEventListener (`click`, handleCardClick)
+restartButton.addEventListener(`click`, restartGame)
 
 // Functions
+function intializeGame(){
+  
+}
+function checkForMatch(){
+
+}
+function updateTimer(){
+
+}
+function displayWinMessage(){
+
+}
+function displayLossMessage(){
+
+}
+function restartGame(){
+
+}
+document.addEventListener(intializeGame)
+
+
